46adf96ae0ed04f074a105d8283554e0c8ef314c
SilaForce <90404958+SilaForce@users.noreply.github.com>
2022-03-06 23:14:20 +0100

    Add files via upload

---
+++ src/screens/basket/basket.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import Header from "../../components/header/admin_header/admin";
+
+const Basket = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default Basket;
