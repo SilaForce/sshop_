8d756ee71265fd48857627f9c44c7d694fd1facb
SilaForce <90404958+SilaForce@users.noreply.github.com>
2022-03-06 13:17:14 +0100

    Add files via upload

---
+++ screens/main/main.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Header from "../../components/header/header";
+import Navbar from "../../components/navbar/navbar";
+
+const Main = () => {
+  return (
+    <div>
+      <Header />
+
+      <Navbar />
+    </div>
+  );
+};
+
+export default Main;
