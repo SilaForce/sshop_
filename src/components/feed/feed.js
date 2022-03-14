0d157f96ad5578ffb68524aa7b6bbfe5451fb4f8
SilaForce <90404958+SilaForce@users.noreply.github.com>
2022-03-14 22:19:31 +0100

    Add files via upload

--- src/components/feed/feed.js
+++ src/components/feed/feed.js
@@ -1,9 +1,31 @@
 import React from "react";
+import Item from "../item/item";
 
 const Feed = () => {
   return (
     <div className="cf">
-      <div className="feed"></div>
+      <div className="feed">
+        <div className="align">
+          <Item />
+          <Item />
+          <Item />
+        </div>
+        <div className="align">
+          <Item />
+          <Item />
+          <Item />
+        </div>
+        <div className="align">
+          <Item />
+          <Item />
+          <Item />
+        </div>
+        <div className="align">
+          <Item />
+          <Item />
+          <Item />
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/row/row.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Navbar from "../navbar/navbar";
+import Feed from "../feed/feed";
+
+function Row() {
+  return (
+    <div className="rowc">
+      <Navbar />
+      <Feed />
+    </div>
+  );
+}
+
+export default Row;
