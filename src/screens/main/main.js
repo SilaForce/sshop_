c5ff71cc5692c7dad341c42828946ed225283bdb
SilaForce <90404958+SilaForce@users.noreply.github.com>
2022-03-14 22:21:40 +0100

    Add files via upload

--- src/screens/main/main.js
+++ src/screens/main/main.js
@@ -2,13 +2,13 @@ import React from "react";
 
 import Header from "../../components/header/header";
 import Navbar from "../../components/navbar/navbar";
+import Row from "../../components/row/row";
 
 const Main = () => {
   return (
     <div>
       <Header />
-
-      <Navbar />
+      <Row />
     </div>
   );
 };
