3a171c59bd855905495b2bfae9164aeb2754bb16
SilaForce <90404958+SilaForce@users.noreply.github.com>
2022-03-17 22:49:39 +0100

    Add files via upload

--- src/components/feed/feed.js
+++ src/components/feed/feed.js
@@ -1,29 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Item from "../item/item";
 
 const Feed = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/items")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setItems(data);
+      });
+  }, []);
+
   return (
     <div className="cf">
       <div className="feed">
         <div className="align">
-          <Item />
-          <Item />
-          <Item />
-        </div>
-        <div className="align">
-          <Item />
-          <Item />
-          <Item />
-        </div>
-        <div className="align">
-          <Item />
-          <Item />
-          <Item />
-        </div>
-        <div className="align">
-          <Item />
-          <Item />
-          <Item />
+          {items &&
+            items.map((item, value) => {
+              return <Item data={item} />;
+            })}
         </div>
       </div>
     </div>
