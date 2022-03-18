ca87ce5ab1263b2cddb2c938b71897334cddd0dd
SilaForce <90404958+SilaForce@users.noreply.github.com>
2022-03-18 21:52:33 +0100

    Add files via upload

--- src/components/item/item.js
+++ src/components/item/item.js
@@ -3,19 +3,16 @@ import React, { useState, useEffect } from "react";
 function Item({ data }) {
   return (
     <div className="item">
+      <div className="icon"></div>
       <div className="details">
         {Object.keys(data).map((attr) => {
           return <div>{attr}</div>;
         })}
       </div>
-      <div>
-        <div className="cell">
-          <div>
-            {Object.values(data).map((attr) => {
-              return <div>{attr}</div>;
-            })}
-          </div>
-        </div>
+      <div className="cell">
+        {Object.values(data).map((attr) => {
+          return <div>{attr}</div>;
+        })}
       </div>
     </div>
   );
