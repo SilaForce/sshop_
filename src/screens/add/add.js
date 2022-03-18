26c5d2069d39b756906be9df1d2a5df831fd4b71
SilaForce <90404958+SilaForce@users.noreply.github.com>
2022-03-18 21:49:40 +0100

    Add files via upload

---
+++ src/screens/add/add.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Header from "../../components/header/admin_header/admin";
+
+function Add() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default Add;
