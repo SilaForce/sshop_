e13d13dd29f913cd278fba5761c3f33232da22c5
Amar Silajdzic <90404958+SilaForce@users.noreply.github.com>
2022-04-06 23:52:58 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Profile from "./screens/profile/profile";
 import Main from "./screens/main/main";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Basket from "./screens/basket/basket";
+import Add from "./screens/add/add";
+import ItemScreen from "./screens/item/index";
 
 const App = () => {
   return (
@@ -13,6 +15,10 @@ const App = () => {
         <Route path="/profile" element={<Profile />}></Route>
 
         <Route path="/profile/basket" element={<Basket />}></Route>
+
+        <Route path="/add" element={<Add />}></Route>
+
+        <Route path="/item" element={<ItemScreen />}></Route>
       </Routes>
     </BrowserRouter>
   );
