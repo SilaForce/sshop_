c9b7f69272269ee25845ddf13708aad52348affa
Amar Silajdzic <90404958+SilaForce@users.noreply.github.com>
2022-04-06 23:57:16 +0200

    Add files via upload

--- src/components/item/item.js
+++ src/components/item/item.js
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 function Item({ data }) {
   return (
-    <div className="item">
-      <div className="icon"></div>
-      <div className="details">
-        <ul className="no-bullets">
-          <li>Name</li>
-          <li>Price</li>
-          <li>Condition</li>
-        </ul>
-      </div>
-      <div className="cell">
-        <div>{data.name}</div>
-        <div>{data.price}</div>
-        <div>{data.condition}</div>
+    <div className="item-wrapper">
+      <div className="item-frame">
+        <div className="name">{data.name}</div>
+        <div className="item-flex">
+          <div className="output-frame">
+            <div className="output">{data.price}</div>
+            <div className="output">{data.condition}</div>
+          </div>
+          <div className="output-frame">
+            <div className="output">{data.category}</div>
+            <div className="output">{data.location}</div>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/item/sitem/sitem.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Sitem({ data }) {
+  const navigate = useNavigate();
+  return (
+    <div className="item" onClick={() => navigate("/item")}>
+      <div className="icon"></div>
+      <div className="details">
+        <ul className="no-bullets">
+          <li>Name</li>
+          <li>Price</li>
+          <li>Condition</li>
+        </ul>
+      </div>
+      <div className="cell">
+        <div>{data.name}</div>
+        <div>{data.price}</div>
+        <div>{data.condition}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Sitem;
