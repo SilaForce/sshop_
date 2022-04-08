475e01b604494cf4c4152de25e379a5b890b48ac
Amar Silajdzic <90404958+SilaForce@users.noreply.github.com>
2022-04-09 00:03:18 +0200

    Add files via upload

--- src/screens/profile/profile.js
+++ src/screens/profile/profile.js
@@ -1,14 +1,10 @@
 import React from "react";
-import { motion } from "framer-motion";
-import "../../components/tippy";
-import Tippy from "@tippyjs/react";
-import Aheader from "../../components/header/admin_header/admin";
+
 import Anavbar from "../../components/navbar/admin_bar/admin";
 
 const Profile = () => {
   return (
     <div>
-      <Aheader />
       <Anavbar />
     </div>
   );
