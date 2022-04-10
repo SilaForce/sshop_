1a5d12a624d4b570c77146d032029a80ccb98f6d
Amar Silajdzic <90404958+SilaForce@users.noreply.github.com>
2022-04-10 17:26:51 +0200

    Add files via upload

--- src/components/item/sitem/sitem.js
+++ src/components/item/sitem/sitem.js
@@ -4,7 +4,12 @@ import { useNavigate } from "react-router-dom";
 function Sitem({ data }) {
   const navigate = useNavigate();
   return (
-    <div className="item" onClick={() => navigate("/item")}>
+    <div
+      className="item"
+      onClick={() => {
+        navigate("/item", { state: data });
+      }}
+    >
       <div className="icon"></div>
       <div className="details">
         <ul className="no-bullets">
