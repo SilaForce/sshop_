65588fcb20e2acd2b13f61a06a8ef49b895256c5
Amar Silajdzic <90404958+SilaForce@users.noreply.github.com>
2022-04-14 00:56:22 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Profile from "./screens/profile/profile";
 import Main from "./screens/main/main";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Basket from "./screens/basket/basket";
 import Add from "./screens/add/add";
 import ItemScreen from "./screens/item/index";
-import MyItems from "./screens/myitems";
 import Header from "./components/header/header";
 
 const App = () => {
@@ -22,8 +22,6 @@ const App = () => {
         <Route path="/add" element={<Add />}></Route>
 
         <Route path="/item" element={<ItemScreen />}></Route>
-
-        <Route path="/myitems" element={<MyItems />}></Route>
       </Routes>
     </BrowserRouter>
   );
